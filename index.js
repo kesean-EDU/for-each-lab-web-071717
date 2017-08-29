7685266c9ec0a0182cd567a2b8139403aef2d2e7
kewoodh <kesean.woodhouse@gmail.com>
2017-08-29 11:21:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function iterativeLog(array){
+  array.forEach(function(element, index){
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback){
+  let arr = [1, 2, 3, 4]
+  arr.forEach(function(element){
+    callback(element)
+  })
+  return arr
+}
+
+function doToArray(array, callback) {
+  array.forEach(function(element){
+    callback(element)
+  })
+}
